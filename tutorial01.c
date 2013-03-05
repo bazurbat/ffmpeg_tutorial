874470ca6dfbd24114e443e6da6dfadf190ee097
Oleg Kolosov <bazurbat@gmail.com>
2013-03-05 09:39:06 +0400

    Corrected error reporting code, adding decoding support

--- tutorial01.c
+++ tutorial01.c
@@ -1,6 +1,7 @@
 #include <glib.h>
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
+#include <stdint.h>
 
 int main (int argc, char *argv[])
 {
@@ -10,41 +11,59 @@ int main (int argc, char *argv[])
 
     av_register_all();
 
-    AVFormatContext *fmt_ctx = NULL;
-
-    if (avformat_open_input (&fmt_ctx, src_filename, NULL, NULL) < 0)
+    AVFormatContext *format_context = NULL;
+    if (avformat_open_input (&format_context, src_filename, NULL, NULL) < 0)
     {
         g_error ("Could not open input");
-        exit (1);
     }
 
-    if (avformat_find_stream_info (fmt_ctx, NULL) < 0)
+    if (avformat_find_stream_info (format_context, NULL) < 0)
     {
-        g_error ("Could not find stream information");
+        g_critical ("Could not find stream information");
         goto end;
     }
 
     int video_stream_idx = -1;
-    if ((video_stream_idx = av_find_best_stream (fmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0)) < 0)
+    if ((video_stream_idx = av_find_best_stream (format_context,
+                                    AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0)) < 0)
     {
-        g_error ("Could not find best stream");
+        g_critical ("Could not find best stream");
         goto end;
     }
 
     g_debug ("video stream index: %d", video_stream_idx);
 
-    av_dump_format (fmt_ctx, 0, src_filename, 0);
+    av_dump_format (format_context, 0, src_filename, 0);
+
+    AVCodecContext *codec_context =
+            format_context->streams[video_stream_idx]->codec;
+    AVCodec *codec = avcodec_find_decoder (codec_context->codec_id);
+    if (!codec)
+    {
+        g_critical ("Could not find codec");
+        goto end;
+    }
+
+    if (avcodec_open2 (codec_context, codec, NULL) < 0)
+    {
+        g_critical ("Could not open codec");
+        goto end;
+    }
 
     AVFrame *frame = avcodec_alloc_frame ();
     if (!frame)
     {
-        g_error ("Could not allocate frame");
+        g_critical ("Could not allocate frame");
         goto end;
     }
 
+    int num_bytes = avpicture_get_size (PIX_FMT_RGB24, codec_context->width,
+                                        codec_context->height);
+    uint8_t *buffer = av_malloc (num_bytes * sizeof (*buffer));
+
     AVPacket pkt;
     int max_size = -1;
-    while (av_read_frame(fmt_ctx, &pkt) >= 0)
+    while (av_read_frame(format_context, &pkt) >= 0)
     {
         if (pkt.stream_index == video_stream_idx)
         {
@@ -60,7 +79,8 @@ int main (int argc, char *argv[])
 
 end:
     av_free (frame);
-    avformat_close_input (&fmt_ctx);
+    avcodec_close (codec_context);
+    avformat_close_input (&format_context);
 
     return 0;
 }
